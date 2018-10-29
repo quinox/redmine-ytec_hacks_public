c2d6b9a04b3dca58e9a50829cd2211051936c65e
Ceesjan Luiten <ceesjan@ytec.nl>
2018-10-29 10:38:19 +0100

    Prettify inline code style

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :ytec_hacks_public do
   name 'YTEC Hacks Public plugin'
   author 'YTEC'
   description 'Some hacks for improving Redmine'
-  version '2018082105'
+  version '2018102900'
   url 'https://github.com/ytec-nl/redmine-ytec_hacks_public'
   author_url 'https://ytec.nl/'
 end
--- lib/ytec_hacks_public/hooks.rb
+++ lib/ytec_hacks_public/hooks.rb
@@ -3,6 +3,7 @@ module YtecHacksPublicViewListener
     def view_layouts_base_body_bottom(context={ })
       tags = [                                             
           stylesheet_link_tag('header_numbers_on_wiki_pages', :plugin => "ytec_hacks_public"),
+          stylesheet_link_tag('inline_code', :plugin => "ytec_hacks_public"),
           javascript_include_tag('hacks', :plugin => "ytec_hacks_public"),
       ]                                                    
       return tags.join("\n")
